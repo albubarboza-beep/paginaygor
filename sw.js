1479c73226ab5740a22eff1887586d65f8a19dbb
albubarboza-beep <albu.barboza@gmail.com>
2026-05-23 13:29:19 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Estratégia: Network First com Fallback para Cache.
  */
 
-const CACHE_NAME = 'ygor-premium-v1.0.1';
+const CACHE_NAME = 'ygor-premium-v1.0.2';
 
 // Arquivos críticos para o primeiro carregamento
 const urlsToCache = [
